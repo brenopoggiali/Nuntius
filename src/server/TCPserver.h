46b0bd7d0124bc8518e002000fe24992b3edf81f
Flavio Lucio <flaviolc18@gmail.com>
2018-10-10 21:41:23 -0300

    Added server implementation

--- src/server/TCPserver.h
+++ src/server/TCPserver.h
@@ -2,11 +2,44 @@
 #define TCP_SERVER_H
 
 #include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define MAXPACKETSIZE 4096
+
+struct arg_struct {
+  
+  auto arg1;
+  auto arg2;
+};
 
 class TCPserver{
 
-public:
-  TCPserver();
-};
+private:
+  int _server_sock;
+  int _client_sock;
+  int _n;
+  int _pid;
+  struct sockaddr_in _server_addr;
+  struct sockaddr_in _client_addr;
+  pthread_t _server_thread;
+  char _buffer[MAXPACKETSIZE];
 
-#endif
+public:
+  TCPserver(int port);
+  void recv_conn();
+  void send_msg();
+  void detach();
+  void clean();
+
+  static void *client_handler(void *args);
+};
