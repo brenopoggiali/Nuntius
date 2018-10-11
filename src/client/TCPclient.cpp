5e4296eea4c63f585cada4fbf42955766a3459c0
Flavio Lucio <flaviolc18@gmail.com>
2018-10-11 17:26:38 -0300

    Added client basic functions

--- src/client/TCPclient.cpp
+++ src/client/TCPclient.cpp
@@ -2,7 +2,33 @@
 
 using namespace std;
 
-TCPclient::TCPclient(){
+TCPclient::TCPclient(string addr, int port){
 
-  cout << "tcp client working" << endl;
+  _m_sock = INVALID_SOCKET;
+  _addr = addr;
+  _port = port;
+
+  memset(&_server_addr, 0, sizeof(_server_addr));
+  _server_addr.sin_family = AF_INET;
+  _server_addr.sin_addr.s_addr = inet_addr(_addr);
+  _server_addr.sin_port = htons(_port);
+}
+
+TCPclient::connect_serv(){
+
+  /* if we already have an incoming connection */
+  if(_m_sock != INVALID_SOCKET){
+    closesocket(_m_sock);
+  }
+
+  _m_sock = socket(AD_INET, SOCK_STREAM, 0);
+
+  cout << "[*] Trying to connect to the server at " << _addr << ":" << _port << endl;
+
+  _n = connect(_m_sock, (struct sockaddr *)&_server_addr, sizeof(_server_addr));
+  if(_n == SOCKET_ERROR){
+    cout << "[*] ERROR: Could not connect to the server" << endl;
+    exit(EXIT_SUCCESS);
+  }
+
 }
--- src/client/TCPclient.h
+++ src/client/TCPclient.h
@@ -2,11 +2,32 @@
 #define TCP_CLIENT_H
 
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netdb.h>
+
+using namespace std;
 
 class TCPclient{
 
-public:
-  TCPclient();
-};
+private:
+  int _m_sock;
+  int _port;
+  int _n;
+  string _addr;
+  struct sockaddr_in _server_addr;
 
+public:
+  TCPclient(string addr, int port);
+  void connect_serv();
+  void handler();
+
+};
 #endif
