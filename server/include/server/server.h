f4fe2a2c08ef78eb9e6d1002bfe78fefbc2da5d9
Flavio <flaviolc18@gmail.com>
2018-11-02 20:31:02 -0300

    implementa classe do server com boost

--- server/include/server/server.h
+++ server/include/server/server.h
@@ -1,48 +1,28 @@
-#ifndef TCP_SERVER_H
-#define TCP_SERVER_H
+#ifndef SERVER_H
+#define SERVER_H
 
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <pthread.h>
+#include <boost/noncopyable.hpp>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <stdint.h>
 
-#define MAXPACKETSIZE 4096
+class client;
+typedef boost::shared_ptr<client> client_ptr;
 
-using namespace std;
-
-struct arg_struct {
-
-  int arg1;
-  string arg2;
-};
-
-class TCPserver{
+class server : private boost::noncopyable {
 
 private:
-  int _server_sock;
-  int _client_sock;
-  int _n;
-  int _pid;
-  struct sockaddr_in _server_addr;
-  struct sockaddr_in _client_addr;
-  pthread_t _server_thread;
-  char _buffer[MAXPACKETSIZE];
+  boost::asio::io_service _io;
+  boost::asio::ip::tcp::acceptor _acceptor;
+  client_ptr _client;
+  int _conn, _max_conn;
+
+  void handle_accept(const boost::system::error_code& e);
 
 public:
-  TCPserver(int port);
+  explicit server(const std::string host, const std::string& port);
   void recv_conn();
-  void send_msg(string msg);
-  void detach();
-  void clean();
 
-  static void *client_handler(void *a);
 };
 
 #endif
--- server/src/server/server.cpp
+++ server/src/server/server.cpp
@@ -1,104 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdint.h>
+#include <sstream>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include <vector>
+
 #include "server/server.h"
+#include "client/client.h"
 
-TCPserver::TCPserver(int port){
+//set up the server acceptor to listen to an endpoint
+server::server(const std::string host, const std::string& port)
+  : _client(new client(_io)), _acceptor(_io) {
 
-  /* create an endpoint for comunication and returns a file descriptor */
-  _server_sock = socket(AF_INET, SOCK_STREAM, 0);
-
-  /* setup the sockaddr_in struct */
-  memset(&_server_addr, 0, sizeof(_server_addr));
-  _server_addr.sin_family = AF_INET;
-  _server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  _server_addr.sin_port = htons(port);
-
-  /* let the reuse of the port after killing the process */
-  int reuse = 1;
-  setsockopt(_server_sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse));
-
-  #ifdef SO_REUSEPORT
-    setsockopt(_server_sock, SOL_SOCKET, SO_REUSEPORT, (const char*)&reuse, sizeof(reuse));
-  #endif
-
-  /* binds the socket to the address and port number of the struct */
-  bind(_server_sock, (struct sockaddr *)&_server_addr, sizeof(struct sockaddr));
-
-  /* listen / waits for connections */
-  listen(_server_sock, 5);
-
-  cout << "[*] Listening on " << port << " ..." << endl;
-
+  _max_conn = 5;
+  _conn = 0;
+  boost::asio::ip::tcp::resolver resolver(_io);
+  boost::asio::ip::tcp::resolver::query query(host, port);
+  boost::asio::ip::tcp::endpoint endpoint = *resolver.resolve(query);
+  _acceptor.open(endpoint.protocol());
+  _acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+  _acceptor.bind(endpoint);
+  _acceptor.listen();
 }
 
-void TCPserver::recv_conn(){
-
-  while(true){
-
-    /* set the client socket size */
-    socklen_t sock_size = sizeof(_client_addr);
-
-    /* accept the client connection */
-    _client_sock = accept(_server_sock, (struct sockaddr*)&_client_addr, &sock_size);
-    cout << "[*] Conection received from " << inet_ntoa(_client_addr.sin_addr) << endl;
-
-    /* to pass more than one arg for a thread routine */
-    arg_struct args;
-    args.arg1 = _client_sock;
-    args.arg2 = string(inet_ntoa(_client_addr.sin_addr));
-
-    /* open a thread and assign to the client_handler routine */
-    pthread_create(&_server_thread, NULL, &client_handler, (void *)&args);
-  }
+//asynchronously accept a client connection and call a callback
+server::recv_conn(){
+  _acceptor.async_accept(_client->socket(), boost::bind(&server::handle_accept, this, boost::asio::placeholders::error));
 }
 
-void *TCPserver::client_handler(void *a){
+//callback to start a client instance and accept more connections
+server::handle_accept(const boost::system::error_code& e){
 
-  arg_struct *args = (arg_struct *)a;
+  if(e){
+    std::clog << "[Error - server::handle_accept] " << e.message() << "." << std::endl;
+  }else{
 
-  int n;
-  int client_sock = (long)args->arg1;
-  string addr = (string)args->arg2;
-  char buffer[MAXPACKETSIZE];
-
-  /* mark the thread as detached to release its resources automatically when it terminates */
-  pthread_detach(pthread_self());
-  while(true){
-
-    /* clear the buffer to receive the msg */
-    memset(buffer, 0, MAXPACKETSIZE);
-
-    /* receive the client message */
-    n = recv(client_sock, buffer, MAXPACKETSIZE, 0);
-    if(n < 0){
-
-      close(client_sock);
-      break;
+    _client->start();
+    _client.reset(new Client(_io));
+    if(_max_conn == 0 || ++_conn < _max_conn){
+      _acceptor.async_accept(_client->socket(), boost::bind(&server::handle_accept, this, boost::asio::placeholders::error));
     }
-    buffer[n] = 0;
-    cout << "[" << addr << "]: " << string(buffer) << endl;
   }
-  return 0;
-}
-
-void TCPserver::send_msg(string msg){
-
-  _n = send(_client_sock, msg.c_str(), msg.length(), 0);
-
-  if(_n != msg.length()){
-
-    cout << "ERROR fail sending message!" << endl;
-    this->detach();
-    exit(EXIT_FAILURE);
-  }
-}
-
-void TCPserver::clean(){
-
-  memset(_buffer, 0, MAXPACKETSIZE);
-  _n = 0;
-}
-
-void TCPserver::detach(){
-
-  close(_server_sock);
-  close(_client_sock);
 }
