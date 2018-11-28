d7559cb469de54c23e49303c9ac9ee9e6d097edb
brenopoggiali <brenopoggiali@gmail.com>
2018-11-28 10:10:08 -0200

    Added more test cases

--- tests/test_exception.cpp
+++ tests/test_exception.cpp
@@ -3,11 +3,22 @@
 #include "exception.h"
 #include "doctest.h"
 
-TEST_CASE("Test Exceptions in Client"){
-	CHECK_NOTHROW(Client c("127.0.0.1", 5555, "client1", "channel_name"));
-	CHECK_THROW(Client c1("127.0.0.1", 5555, "client1", "channel_name"));
-}
 TEST_CASE("Test Exceptions in Server"){
 	CHECK_NOTHROW(Server s(5555));
-	CHECK_THROW(Server s2(5555));
+	try{
+		Server s2(5555);
+	}
+	catch(std::exception &e){
+		CHECK_EQ(e.what() == "Port already in use");
+	}
+}
+TEST_CASE("Test Exceptions in Client"){
+	Server s(5555);
+	CHECK_NOTHROW(Client c("127.0.0.1", 5555, "client1", "channel_name"));
+	try{
+	 Client c1("127.0.0.1", 5555, "client1", "channel_name");
+	}
+	catch(std::exception &e){
+		CHECK_EQ(e.what() == "Nickname already in use");
+	}
 }
